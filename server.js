e7790ba85e823eef10534bc78571d33020c2b02b
lawyerh <lawyerh_01@yahoo.com>
2018-05-15 13:18:01 -0700

    Figured out MySQL connection

--- server.js
+++ server.js
@@ -5,17 +5,33 @@ const bp = require('body-parser');
 const mysql = require('mysql');
 const mySQLConnect = require('./connection');
 
-const connection = mysql.createConnection({mySQLConnect});
+const connection = mysql.createConnection({
+    host: mySQLConnect.host,
+    user: mySQLConnect.user,
+    password: mySQLConnect.password,
+    port: mySQLConnect.port,
+    database: mySQLConnect.database
+});
 
 const app = express();
 
 app.use(bp.json());
 
 app.get('/', function(req,res){
-    const response = {
-        hello: "Welcome to the database api!"
-    }
-    res.json(response)
+
+    connection.connect(function(err){
+        if(err){
+            throw err;
+        }
+        connection.query("select * from posts;", function(err, results){
+            if(err){
+                throw err;
+            }
+            else{
+                res.json(results);
+            }
+        });
+    })
 })
 
 app.listen(4000, function(){
